57d8bee45dc9bc620fcc518ef8974c3da0cec74b
Otávio Azevedo <otavioad1997@gmail.com>
2022-05-19 22:22:50 +0200

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from '../pages/Carregando';
 
@@ -23,6 +24,9 @@ class Header extends React.Component {
         ) : (
           <nav data-testid="header-user-name">{ name }</nav>
         )}
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
       </header>
     );
   }

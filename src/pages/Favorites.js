31ae2acd463b3ad56095beb3be73f987938051f9
Otávio Azevedo <otavioad1997@gmail.com>
2022-11-27 14:26:25 -0300

    Requisito 12

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -2,32 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
 
 class Favorites extends React.Component {
   state = {
     checked: false,
+    loading: false,
     favorites: [],
   }
 
   async componentDidMount() {
+    this.getFavorite();
+  }
+
+  async componentDidUpdate() {
+    this.getFavorite();
+  }
+
+  getFavorite = async () => {
     const newArray = await getFavoriteSongs();
     this.setState({ favorites: newArray });
   }
 
+  onInputChange = async (music) => {
+    this.setState({ checked: false, loading: true });
+    await removeSong(music);
+    const arrayMusicsStorage = await getFavoriteSongs();
+    this.setState({ loading: false });
+    return this.setState({ favorites: arrayMusicsStorage });
+  }
+
   render() {
-    const { checked, favorites } = this.state;
+    const { checked, favorites, loading } = this.state;
     return (
       <div>
         <Header />
-        <div data-testid="page-favorites">Favorites</div>
-        {favorites.map((music) => (
-          <MusicCard
-            key={ music.previewUrl }
-            music={ music }
-            checkbox={ checked }
-          />
-        ))}
+        { loading === true ? <Carregando />
+          : (
+            <div data-testid="page-favorites">
+              Favorites
+              {favorites.map((music) => (
+                <MusicCard
+                  key={ music.previewUrl }
+                  music={ music }
+                  checkbox={ checked }
+                  onChange={ () => this.onInputChange }
+                />
+              ))}
+            </div>)}
       </div>
     );
   }
